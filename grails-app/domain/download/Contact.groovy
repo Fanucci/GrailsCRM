5d7983c833640bbf8498902f7890493274a8627c
Dmitry Sorokin <Dmitry Sorokin@192.168.1.3>
2016-06-14 19:33:01 +0500

    new2

--- grails-app/domain/download/Contact.groovy
+++ grails-app/domain/download/Contact.groovy
@@ -5,34 +5,44 @@ class Contact {
     ContactField CF,CF1,CF2,CF3,CF4,CF5,CF6,CF7,CF8,CF9,CF10,CF11,CF12,CF13,CF14,CF15,CF16,CF17,CF18,CF19,CF20
     int size
     List<ContactField> CFList
-   def dynamicProperties= [:]
-   //setter
-   def propertyMissing(String name, value) { dynamicProperties[name] = value }
-   //getter
-   def propertyMissing(String name) { dynamicProperties= [name] }
+
         
 public Contact(){
 listOfFields = UserSettings.getUserFieldSettings()
 size=listOfFields.size()
-    addCFtoList(size)
-    int i=0
-    for (ContactField ContF:CFList){
-        ContF=listOfFields.get(i)
-        println(ContF.getFieldName()+" = "+listOfFields.get(i).getFieldName())
+    addCFtoList(size, CF,CF1,CF2,CF3,CF4,CF5,CF6,CF7,CF8,CF9,CF10,CF11,CF12,CF13,CF14,CF15,CF16,CF17,CF18,CF19,CF20)
+   /* int i=0
+    Iterator<String> crunchifyIterator = CFList.iterator();
+    while (crunchifyIterator.hasNext()) {
+        crunchifyIterator.next()=listOfFields.get(i)
         i++
     }
+*/
+    println(CF.getFieldName())
 }
 
-   public addCFtoList(int i){
-       CFList = new ArrayList<ContactField>()
-       Collections.addAll(CFList, CF,CF1,CF2,CF3,CF4,CF5,CF6,CF7,CF8,CF9,CF10,CF11,CF12,CF13,CF14,CF15,CF16,CF17,CF18,CF19,CF20);
-       int s=CFList.size()
-       for(int x=i;x<s;x++)CFList.remove(i)
+   public addCFtoList(int i,ContactField...v){
+        CFList = new ArrayList<ContactField>()
+        int z=0
+       for (ContactField x:v){
+        if(i>0){
+            x=listOfFields.get(z)
+                CFList.add(x);
+            z++
+            i--
+        }}
+
    }
    
    public List<ContactField> getNewBaseFields(){return CFList}
 
- /*   def writeContactFields(){
+ /*  def dynamicProperties= [:]
+   //setter
+   def propertyMissing(String name, value) { dynamicProperties[name] = value }
+   //getter
+   def propertyMissing(String name) { dynamicProperties= [name] }
+   
+        def writeContactFields(){
     for(ContactField CF:listOfFields){
         this."ip${CF.getFieldName()}" = CF.getFieldProperty()
     }
--- grails-app/domain/download/ContactField.groovy
+++ grails-app/domain/download/ContactField.groovy
@@ -1,5 +1,7 @@
 package download
 
+import java.lang.reflect.Field
+
 class ContactField {
 String name
 String property
@@ -23,6 +25,10 @@ public ContactField(String n,String p,int tp,Type t){
     this.tablePosition=tp
     this.sd=t
 }
+public copyObjectParams(ContactField CFs){
+        this=CFs
+}
+    
 public String getFieldName(){return this.name}
 public void setFieldName(String name){this.name=name}
     
@@ -33,7 +39,7 @@ public int getFieldTablePosition(){return this.tablePosition}
 public void setFieldTablePosition(int tablePosition){this.tablePosition=tablePosition}
 
 public Type getFieldType(){return this.sd}
-public void setFieldType(Type sd){this.sd=sd}    
+public void setFieldType(Type sd){this.sd=sd}
     static constraints = {
     }
 }
